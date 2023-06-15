5d20aa0abd3ebb56daa478afa667f6ee5fcc48c4
Sachit Nanajkar <sachitnjk@gmail.com>
2023-06-15 17:56:07 +0530

    Revert "SwitchTrigger_ Added switch trigger"
    This reverts commit 9f6fc2d10376d57f93ad852c6ab298b4140c94a6.

--- Assets/Scripts/SwitchScripts/SwitchTrigger.cs
+++ Assets/Scripts/SwitchScripts/SwitchTrigger.cs
@@ -6,10 +6,6 @@ public class SwitchTrigger : MonoBehaviour
 {
 	private PlayerInputControls playerInputControls;
 
-	[SerializeField] private float clickDelay;
-
-	private float currentDelay = 0f;
-	 
 	private void Start()
 	{
 		playerInputControls = GetComponent<PlayerInputControls>();
@@ -21,19 +17,13 @@ public class SwitchTrigger : MonoBehaviour
 
 	void InteractCheck()
 	{
-		if(currentDelay > 0f) 
-		{
-			currentDelay -= Time.deltaTime;
-		}
-
-		if(currentDelay <= 0f && playerInputControls.leftClick.WasPressedThisFrame())
+		if(playerInputControls.leftClick.WasPressedThisFrame())
 		{
 			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
 			if(Physics.Raycast(ray, out RaycastHit hitInfo) && hitInfo.collider == GetComponent<Collider>()) 
 			{
 				Debug.Log("Click is detected");
-				currentDelay = clickDelay;
 			}
 		}
 	}
